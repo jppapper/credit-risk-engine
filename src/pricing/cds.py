765c96173b630eaa4a1c1ed6984bd64bb6070208
jppapper <your@email.com>
2026-04-03 18:29:25 -0400

    feat: complete CDSAnalytics class with upfront, cs01, ir01, carry, jump to default

---
+++ src/pricing/cds.py
@@ -0,0 +1,93 @@
+import numpy as np
+import datetime
+
+class CDSAnalytics:
+    def __init__(self, instrument_id: str, maturity_date: str, coupon_bps: float, notional: float, recovery_rate: float = 0.40):
+        self.instrument_id = instrument_id
+        self.maturity_date = maturity_date
+        self.coupon_bps = coupon_bps
+        self.notional = notional
+        self.recovery_rate = recovery_rate
+
+    def calc_hazard_rate(self, spread_bps: float):
+        hazard_rate = (spread_bps/10000)/(1-self.recovery_rate)
+        return hazard_rate
+    
+    def calc_survival_probability(self, hazard_rate: float, end_date: str):
+        start_date = datetime.date.today()
+        end_date = datetime.date.fromisoformat(end_date)
+        t= (end_date - start_date).days / 365
+        survival_probability = np.exp(-hazard_rate*t)
+        return survival_probability
+    
+    def calc_discount_factors(self, rate: float, end_date: str):
+        start_date = datetime.date.today()
+        end_date = datetime.date.fromisoformat(end_date)
+        t= (end_date - start_date).days / 365
+        discount_factor = np.exp(-rate*t)
+        return discount_factor
+
+    def _build_payment_dates(self, end_date: str) -> list:
+        payment_dates = []
+        start_date = datetime.date.today()
+        end_date = datetime.date.fromisoformat(end_date)
+        current_date = start_date
+        while current_date < end_date:
+            current_date = current_date + datetime.timedelta(days=90)
+            payment_dates.append(current_date)
+        return payment_dates        
+
+
+    def calc_protection_leg(self, par_coupon: float, end_date: str, rate: float):
+        hazard_rate = self.calc_hazard_rate(par_coupon)
+        payment_dates = self._build_payment_dates(end_date)
+        protection_leg = 0
+        prev_survival_rate = 1
+        for date in  payment_dates:
+            current_survival_rate = self.calc_survival_probability(hazard_rate, date.isoformat())
+            default_probability = prev_survival_rate - current_survival_rate
+            df = self.calc_discount_factors(rate, date.isoformat())
+            protection_leg += self.notional * (1 - self.recovery_rate) * default_probability * df
+            prev_survival_rate = current_survival_rate 
+        return protection_leg
+    
+    def calc_premium_leg(self, par_coupon: float, end_date: str, rate: float):
+        hazard_rate = self.calc_hazard_rate(par_coupon)
+        payment_dates = self._build_payment_dates(end_date)
+        premium_leg = 0
+        for date in  payment_dates:
+            current_survival_rate = self.calc_survival_probability(hazard_rate, date.isoformat())
+            df = self.calc_discount_factors(rate, date.isoformat())
+            premium_leg += self.notional * current_survival_rate * df * (self.coupon_bps / 10000) * 0.25
+        return premium_leg
+    
+    def calc_upfront(self, par_coupon: float, end_date: str, rate: float):
+        protection_leg = self.calc_protection_leg(par_coupon, end_date, rate)
+        premium_leg = self.calc_premium_leg(par_coupon, end_date, rate)
+        upfront = protection_leg - premium_leg
+        return(upfront)
+    
+    def calc_cs01(self, par_coupon: float, end_date: str, rate: float):
+        up_upfront = self.calc_upfront(par_coupon + 1, end_date, rate)
+        down_upfront = self.calc_upfront(par_coupon - 1, end_date, rate)
+        cs01 = (up_upfront - down_upfront)/2
+        return cs01
+    
+    def calc_ir01(self, par_coupon: float, end_date: str, rate: float):
+        up_upfront = self.calc_upfront(par_coupon, end_date, rate + .0001)
+        down_upfront = self.calc_upfront(par_coupon, end_date, rate - .0001)
+        ir01 = (up_upfront - down_upfront)/2
+        return ir01
+    
+    def calc_jump_to_default(self, par_coupon: float, end_date: str, rate: float):
+        upfront = self.calc_upfront(par_coupon, end_date, rate)
+        protection_payment = self.notional * (1 - self.recovery_rate)
+        jump_to_default = protection_payment - upfront
+        return jump_to_default
+    
+    def calc_carry(self, par_coupon: float):
+        carry = ((self.coupon_bps - par_coupon) / 10000) * self.notional * 1/360
+        return carry
+    
+
+
---
+++ src/streaming/producer.py
@@ -0,0 +1,78 @@
+"""
+Market Data Simulator
+_____________________
+
+Generates realistic spread and rate data for 50k instruments 
+and streams to Kafka topics partitioned by product type.
+"""
+import json
+import random
+import time
+import numpy as np
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+TOPIC_CDS = os.getenv("KAFKA_TOPIC_CDS", "market-data-cds")
+TOPIC_BONDS = os.getenv("KAFKA_TOPIC_BONDS", "market-data-bonds")
+NUM_INSTRUMENTS = int(os.getenv("NUM_INSTRUMENTS", 50000))
+
+class MarketDataSimulator:
+    def __init__(self, num_instruments: int):
+        self.num_instruments = num_instruments
+        self.producer = Producer({"bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS})
+        self.instruments = []
+
+    def build_universe(self):
+        for i in range (self.num_instruments):
+            product = "CDS" if i % 2 == 0 else "BOND"
+            self.instruments.append({
+                "instrument_id": f"{product}-{i:05d}",
+                "product": product,
+                "spread_bps": random.uniform(50, 500),   # initial spread
+                "rate": random.uniform(0.03, 0.07),       # initial rate
+                "maturity_years": random.choice([1, 2, 3, 5, 7, 10]),
+                "notional": random.choice([1_000_000, 5_000_000, 10_000_000, 25_000_000])
+            })
+        return self.instruments
+    
+    def simulate_market_movement(self, instrument: dict) -> dict:
+        rate_move = np.random.normal(0, 0.001)
+        spread_move = np.random.normal(0, 2)
+
+        instrument["spread_bps"] = max(1, instrument["spread_bps"] + spread_move)
+        instrument["rate"] = max(0.001, instrument['rate'] + rate_move)
+        instrument["timestamp"] = time.time()
+        return instrument
+    
+    def delivery_report(self, err, msg):
+        if err:
+            print(f"Delivery failed: {err}")
+    
+    def run_simulator(self, ticks: int=100):
+        for tick in range(ticks):
+            for instrument in self.instruments:
+                updated = self.simulate_market_movement(instrument)
+                topic = TOPIC_CDS if updated["product"] == "CDS" else TOPIC_BONDS
+                self.producer.produce(
+                    topic=topic,
+                    key=updated["instrument_id"],
+                    value=json.dumps(updated),
+                    callback=self.delivery_report
+                )
+
+            self.producer.poll(0)
+            time.sleep(0.1)
+
+        self.producer.flush()
+
+if __name__ == "__main__":
+    simulator = MarketDataSimulator(num_instruments=NUM_INSTRUMENTS)
+    simulator.build_universe()
+    simulator.run_simulator()
+
+
+
